f59ea0a121a3632381a3073b0c3b149a926261d5
xydoy92 <xydoy92@gmail.com>
2020-09-19 16:26:06 +0300

    4 task solved

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -84,7 +84,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+	return value.slice(0, 1);
 }
 
 /**
