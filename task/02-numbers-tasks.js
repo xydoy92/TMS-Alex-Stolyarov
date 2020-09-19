b141c0a78cc43ebfb20d99e9e0db5d390f272186
xydoy92 <xydoy92@gmail.com>
2020-09-19 23:51:16 +0300

    4 number task solved

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -58,7 +58,7 @@ function getAverage(value1, value2) {
 }
 
 /**
- * Returns a distance beetween two points by cartesian coordinates.
+ * Returns a distance between two points by cartesian coordinates.
  *
  * @param {number} x1
  * @param {number} y1
@@ -73,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+	return Math.hypot(x2 - x1, y2 - y1);
 }
 
 /**
