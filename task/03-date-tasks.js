69e39eeb9eb2b503fc3ccef73657c7ef0d011397
xydoy92 <xydoy92@gmail.com>
2020-09-20 17:57:42 +0300

    3 date task solved

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+ return !(date.getFullYear()%400) || (date.getFullYear()%100 && !(date.getFullYear()%4)) ? true : false;
 }
 
 
