43be1ed5025b8ea4e86d8bb07559562880b1f7d3
xydoy92 <xydoy92@gmail.com>
2020-10-01 16:11:47 +0300

    4 defaults task fixed

--- destructuring/test/defaults.js
+++ destructuring/test/defaults.js
@@ -18,7 +18,7 @@ describe('When destructuring you can also provide default values', () => {
       assert.equal(b, 2);
     });
     it('if the value is undefined', () => {
-      const {a, b} = {a: 1, b: void 0};
+      const {a, b = 2} = {a: 1, b: void 0};
       assert.strictEqual(b, 2);
     });
     it('also a string works with defaults', () => {
