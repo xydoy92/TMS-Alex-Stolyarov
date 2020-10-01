11d5ae1e5a0cf472d03e431363d6205ae2827ff2
xydoy92 <xydoy92@gmail.com>
2020-10-01 17:09:27 +0300

    1 string test fixed

--- destructuring/test/string.js
+++ destructuring/test/string.js
@@ -6,7 +6,7 @@ var assert = require('assert');
 
 describe('Destructuring also works on strings', () => {
   it('destructure every character, just as if the string was an array', () => {
-    let a, b, c = 'abc';
+    let [a, b, c] = 'abc';
     assert.deepEqual([a, b, c], ['a', 'b', 'c']);
   });
   it('missing characters are undefined', () => {
