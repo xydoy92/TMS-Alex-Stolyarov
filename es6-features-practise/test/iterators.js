a976329be3af1e8eef259d05d2cb0947a32cfe02
xydoy92 <xydoy92@gmail.com>
2020-10-02 21:58:19 +0300

    14 iterators test fixed

--- es6-features-practise/test/iterators.js
+++ es6-features-practise/test/iterators.js
@@ -112,7 +112,9 @@ describe('A simple iterable without items inside, implementing the right protoco
     });
   });
 
-  let iterable = {};
+  let iterable = {
+	  [Symbol.iterator]: iteratorFunction,
+  };
   beforeEach(function() {
     iterable;
   });
