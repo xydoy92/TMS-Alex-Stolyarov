f17921c73c4fa406af063595661c984b8cefa818
xydoy92 <xydoy92@gmail.com>
2020-10-02 23:22:48 +0300

    4 templateStrings test fixed

--- es6-features-practise/test/templateStrings.js
+++ es6-features-practise/test/templateStrings.js
@@ -28,7 +28,7 @@ describe('A template string, is wrapped in ` (backticks) instead of \' or "', fu
 
   describe('can evaluate any expression, wrapped inside "${...}"', function() {
     it('all inside "${...}" gets evaluated', function() {
-      var evaluated = `${ x } + ${ y }`;
+      var evaluated = `${x + y}`;
       assert.equal(evaluated, x+y);
     });
     it('inside "${...}" can also be a function call', function() {
